954edd2256dc8df34b01354998881ab0b831a54e
Andrew Boardman <acb95@cam.ac.uk>
2023-07-05 09:18:27 +0100

    add integration with mutants file

--- aggregate_mutants.py
+++ aggregate_mutants.py
@@ -7,13 +7,16 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-i','--input',action='store',help='Input directory containing relaxed structures')
     parser.add_argument('-o','--output',action='store',help='Output directory for mutant decoys and scores')
+    parser.add_argument('-m','--mutants',action='store',help='Mutants file')
     args = parser.parse_args()
-    mutants = []
+    ddg_results = []
     for file in os.listdir(args.input):
         if file.endswith('.csv') and len(open(f"{args.input}/{file}",'r').readlines()) > 0:
-            mutants.append(pd.read_csv(f"{args.input}/{file}"))
+            ddg_results.append(pd.read_csv(f"{args.input}/{file}"))
 
-    ddg_results = pd.concat(mutants,axis=0)
+    ddg_results = pd.concat(ddg_results,axis=0)
+
+    mutants = pd.read_csv(args.mutants)
     ddg_results = ddg_results.merge(mutants,on = 'rosetta_mutant',how = 'right')
     ddg_results.to_csv(args.output,index=False)
 
